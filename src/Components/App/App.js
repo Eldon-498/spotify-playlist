7b1270daaf5c8a4272fc70801039e84d63baa4a0
Daniel Eldon <eldondaniel2@gmail.com>
2022-06-16 19:10:49 +0300

    Playlist update

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,22 +3,62 @@ import "./App.css";
 import SearchResults from "../SearchResults/SearchResults";
 import Playlist from "../Playlist/Playlist";
 import SearchBar from "../SearchBar/SearchBar";
+import TrackList from '../TrackList/TrackList';
 
 
 
-class App extends React.Component {
-    constructor(props){
+class App extends React.Component { 
+
+      constructor(props){
       super(props);
       this.state ={ 
       searchResults: [{
-        name: 'Love You\'reself',
+        name: 'Love Yourself',
         artist: 'Justin Beiber', 
-        album: 'Purpose', 
-        id: 1},{name: 'Hello',artist: 'Adele', album: 'Broken', id: 5 },{name: 'Something Stupid', artist: 'Breaking Bad', 
-            album: 'HeisenBerg', 
+        album: 'Love Yourself', 
+        id: 1},
+        {
+          name: 'In Your Arms',
+          artist: 'Illenium, X Ambassadors', 
+          album: 'Ascend', 
+          id: 3}
+        ,{name: 'Hello',artist: 'Adele', album: '25', id: 5 },{name: 'Something Stupid', artist: 'Robbie Williams, Nicole Kidman', 
+            album: 'Weihnachten 2019', 
             id: 11}
-    ]};
+    ],
+      playlistName : "Lucky",
+      playlistTracks : [{
+      name: 'Love Yourself',
+      artist: 'Justin Beiber', 
+      album: 'Love Yourself', 
+      id: 1},{name: 'Hello',artist: 'Adele', album: '25', id: 5 },{name: 'Something Stupid', artist: 'Robbie Williams, Nicole Kidman', 
+          album: 'Weihnachten 2019', 
+          id: 11}
+  ]};
+    this.addTrack =this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName(this);
+    };
+
+    addTrack(track){
+      
+      if (this.state.playlistTracks.find(savedTrack => savedTrack.id === track.id)) {
+        return;
+      } 
+      this.state.playlistTracks.push(track);
+      this.setState({playlistTracks: this.state.playlistTracks});
+    
     }
+
+    removeTrack(track){
+        this.playlistTracks.filter(currentTrack => currentTrack.id !== track.id);
+        this.setState({playlistTracks: this.playlistTracks});
+    }
+
+    updatePlaylistName(name) {
+     this.setState({playlistName: name});
+    }
+
   
     render(){
       return (
@@ -27,8 +67,10 @@ class App extends React.Component {
       <div className="App">
         <SearchBar />
         <div className="App-playlist">
-        <SearchResults searchResults={this.state.searchResults} />
-        <Playlist/>
+        <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
+        <Playlist playlistName ={this.state.playlistName} playlistTracks={this.state.playlistTracks} 
+        onRemove={this.removeTrack} 
+        onNameChange={this.updatePlaylistName} />
         </div>
       </div>
     </div>
